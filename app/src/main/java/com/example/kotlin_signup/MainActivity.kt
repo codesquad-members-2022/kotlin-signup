70e2fd3d594b6e9dc0a094b5a3cbdea23dce54c5
hwj <jminie@naver.com>
2022-03-30 14:33:59 +0900

    feat : 비밀번호와 비밀번호 확인 일치여부 확인기능 구현

--- app/src/main/java/com/example/kotlin_signup/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_signup/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.kotlin_signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.util.Log
 import android.view.View
 import com.example.kotlin_signup.databinding.ActivityMainBinding
@@ -10,6 +12,7 @@ class MainActivity : AppCompatActivity() {
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -29,6 +32,30 @@ class MainActivity : AppCompatActivity() {
                 binding.passwordTextInputEditText.hint = ""
             } else {
                 binding.passwordTextInputEditText.hint = resources.getString(R.string.password_hint)
+            }
+        }
+
+        binding.passwordRecheckTextInputEditText.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus) {
+                binding.passwordRecheckTextInputLayout.editText?.addTextChangedListener(listener)
+            }
+        }
+    }
+
+    private val listener = object : TextWatcher {
+        override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+        }
+
+        override fun afterTextChanged(s: Editable?) {
+            if (s != null) {
+                if (binding.passwordRecheckTextInputLayout.editText?.text.toString() != binding.passwordTextInputLayout.editText?.text.toString()) {
+                    binding.passwordRecheckTextInputLayout.error = "비밀번호가 일치하지 않습니다"
+                } else {
+                    binding.passwordRecheckTextInputLayout.error = null
+                }
             }
         }
     }
